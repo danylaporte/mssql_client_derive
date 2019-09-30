cb224c3192c863ef25536c39498d15f117bf7e77
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-30 15:04:45 +0000

    Deploy danylaporte/mssql_client_derive to github.com/danylaporte/mssql_client_derive.git:gh-pages

---
+++ search-index.js
@@ -0,0 +1,4 @@
+var N=null,E="",T="t",U="u",searchIndex={};
+
+searchIndex["mssql_client_derive"]={"doc":"A derive proc macro that allow a struct to be read from a…","i":[[24,"Sql","mssql_client_derive",E,N,N]],"p":[]};
+initSearch(searchIndex);addSearchOptions(searchIndex);
